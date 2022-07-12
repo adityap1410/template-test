07b06583970ae3e87a47f4d46d52fcf3c11665fc
adityap1410 <89732256+adityap1410@users.noreply.github.com>
2022-07-12 17:42:40 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader=document.getElementById('loader');
+
+//show loading
+function loading(){
+    loader.hidden =false;
+    quoteContainer.hidden=true;
+}
+
+//hide loading
+function complete(){
+    if(!loader.hidden){ 
+    quoteContainer.hidden=false;
+    loader.hidden=true;
+}}
+
+
+//get qute from api
+
+async function getQuote(){
+    loading();
+    const proxyUrl='https://whispering-tor-04671.herokuapp.com/'
+    const apiurl='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response=await fetch(proxyUrl + apiurl);
+        const data=await response.json();
+        //if no author show author as unknown
+        if(data.quoteAuthor===''){
+            authorText.innerText='Unknown';
+        }
+        else{
+            authorText.innerText=data.quoteAuthor;
+        }
+        if(data.quoteText.length>120){
+            quoteText.classList.add('long-quote');
+        }
+        else{
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText=data.quoteText;
+    
+        //stop loader
+        complete();
+    }
+        catch (error) {
+            getQuote();
+        }
+    }
+//tweet quote
+    function tweetQuote(){
+        const quote=quoteText.innerText;
+        const author = authorText.innerText;
+        const twitterUrl =`https://twitter.com/intent/tweet?text=${quote}-${author}`;
+        window.open(twitterUrl, '_blank');
+    }
+//event listeners
+newQuoteBtn.addEventListener('click',getQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+//on load
+getQuote();
